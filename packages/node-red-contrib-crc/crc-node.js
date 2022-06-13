d6a5e941482d40ad3327658ef05d2baedf1d7df0
Stephen Houser <stephenhouser@gmail.com>
2022-06-13 16:58:09 -0400

    add dynamic algorithim selection

--- packages/node-red-contrib-crc/crc-node.js
+++ packages/node-red-contrib-crc/crc-node.js
@@ -16,18 +16,20 @@ module.exports = function(RED) {
 		node.mode = 'calculate'; // 'calculate' or 'validate'
 
 		node.on('input', function(msg, send, done) {
-			if (crc[node.algorithm] !== undefined) {
-				const crc_function = crc[node.algorithm];
+			const algorithm = msg.algorithm ? msg.algorithm : node.algorithm;
+
+			if (crc[algorithm] !== undefined) {
+				const crc_function = crc[algorithm];
 				const crc_computed = crc_function(msg.payload);
 
 				if (node.mode === 'calculate') {
-					msg[node.algorithm] = crc_computed;
+					msg[algorithm] = crc_computed;
 					send(msg);
 				} else if (node.mode === 'validate') {
-					if (msg[node.algorithm] === crc_computed) {
+					if (msg[algorithm] === crc_computed) {
 						send(msg);						
 					} else {	// drop message if CRC does not match.
-						this.debug(`Message's CRC (${node.algorithm}) ${msg[node.algorithm]} does not match computed ${crc_computed}.`);
+						this.debug(`Message's CRC (${algorithm}) ${msg[algorithm]} does not match computed ${crc_computed}.`);
 					}
 				}
 			} else {
