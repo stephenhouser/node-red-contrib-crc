1239878be74ae8bc97d5544161dc12dfcd2ea8fe
Stephen Houser <stephenhouser@gmail.com>
2022-06-13 15:09:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# node-red-contrib-crc
+
+A [NodeRed](https://nodered.org) node to check or compute CRC and checksum values of `msg.payload`
+
+![Example NodeRed Flow](compute-crc-example.png)
+
---
+++ packages/node-red-contrib-crc/crc-node.js
@@ -0,0 +1,42 @@
+/* wsjt-x-decode.js - NodeRed node for decoding WSJT-X messages
+ *
+ * An out of date reference. 
+ * https://github.com/roelandjansen/wsjt-x/blob/master/NetworkMessage.hpp
+ * Consult the source for more accurate details:
+ * https://www.physics.princeton.edu/pulsar/k1jt/wsjtx.html
+ *
+ * 2021/10/11 Stephen Houser, MIT License
+ */
+const wsjtx = require('./wsjt-x-parser');
+
+module.exports = function(RED) {
+	'use strict';
+
+	function CRCNode(config) {
+		RED.nodes.createNode(this, config);
+
+		const node = this;
+		node.name = config.name;
+		node.algorithm = config.algorithm;
+
+		node.on('input', function(msg, send, done) {
+			const decoded = wsjtx.decode(msg.payload);
+			if (decoded && send) {
+				const message = {
+					...msg,
+					topic: decoded.type,
+					payload: decoded,
+					input: msg.payload
+				};
+
+				send(message);
+			}
+
+			if (done) {
+				done();
+			}
+		});
+	}
+
+	RED.nodes.registerType('crc', CRCNode);
+};
