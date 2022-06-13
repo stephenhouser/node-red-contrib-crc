1fb7cb0c2833bf843b647e458058eeccfb138204
Stephen Houser <stephenhouser@gmail.com>
2022-06-13 16:05:43 -0400

    initial working version

--- packages/node-red-contrib-crc/crc-node.js
+++ packages/node-red-contrib-crc/crc-node.js
@@ -1,13 +1,8 @@
-/* wsjt-x-decode.js - NodeRed node for decoding WSJT-X messages
+/* crc-node.js - NodeRed node for checking and calculating CRC values
  *
- * An out of date reference. 
- * https://github.com/roelandjansen/wsjt-x/blob/master/NetworkMessage.hpp
- * Consult the source for more accurate details:
- * https://www.physics.princeton.edu/pulsar/k1jt/wsjtx.html
- *
- * 2021/10/11 Stephen Houser, MIT License
+ * 2022/06/13 Stephen Houser, MIT License
  */
-const wsjtx = require('./wsjt-x-parser');
+const crc = require('crc');
 
 module.exports = function(RED) {
 	'use strict';
@@ -18,18 +13,25 @@ module.exports = function(RED) {
 		const node = this;
 		node.name = config.name;
 		node.algorithm = config.algorithm;
+		node.mode = 'calculate'; // 'calculate' or 'validate'
 
 		node.on('input', function(msg, send, done) {
-			const decoded = wsjtx.decode(msg.payload);
-			if (decoded && send) {
-				const message = {
-					...msg,
-					topic: decoded.type,
-					payload: decoded,
-					input: msg.payload
-				};
+			if (crc[node.algorithm] !== undefined) {
+				const crc_function = crc[node.algorithm];
+				const crc_computed = crc_function(msg.payload);
 
-				send(message);
+				if (node.mode === 'calculate') {
+					msg[node.algorithm] = crc_computed;
+					send(msg);
+				} else if (node.mode === 'validate') {
+					if (msg[node.algorithm] === crc_computed) {
+						send(msg);						
+					} else {	// drop message if CRC does not match.
+						this.debug(`Message's CRC (${node.algorithm}) ${msg[node.algorithm]} does not match computed ${crc_computed}.`);
+					}
+				}
+			} else {
+				this.error('Invalid CRC algorithm selected.');
 			}
 
 			if (done) {
